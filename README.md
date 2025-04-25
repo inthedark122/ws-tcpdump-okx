c7e34741b324220f8e3f03186518e23d02c7e6e0
inthedark122 <inthedark1225@gmail.com>
2025-04-25 14:50:33 +0800

    - add sleep after disconnection - update documentation

--- README.md
+++ README.md
@@ -9,7 +9,10 @@ docker build -t ws-tcpdump-okx .
 ## Run image
 
 ```sh
-docker run --net=host --dns=8.8.8.8 --dns=1.1.1.1 ws-tcpdump-okx
+# Clear run
+docker run ws-tcpdump-okx
+# With DNS
+docker run --dns=8.8.8.8 --dns=1.1.1.1 ws-tcpdump-okx
 ```
 
 Note: you need to remove the container manually after getting the dump
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,5 @@ sleep 2
 echo "[*] Connecting to OKX WebSocket..."
 websocat --ping-interval=30 -vtn "wss://ws.okx.com:8443/ws/v5/public" <<< '{"op": "subscribe","args": [{"channel": "trades", "instId": "PRCL-USDT-SWAP"},{"channel": "trades", "instId": "PUFFER-USDT-SWAP"},{"channel": "trades", "instId": "PYTH-USDT-SWAP"},{"channel": "trades", "instId": "QTUM-USDT-SWAP"},{"channel": "trades", "instId": "RAY-USDT-SWAP"}]}'
 
+echo "[*] Sleep 10 seconds after websocat disconnection"
+sleep 10
